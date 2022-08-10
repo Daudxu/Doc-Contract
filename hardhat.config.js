e40e7d67acbec1d09537fc8d793ce04b49b0829a
xudan <xudan@apowo.com>
2022-08-10 13:06:51 +0800

    fix update

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,13 +7,17 @@ require('dotenv').config();
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.PRI_KEY],
-    },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [process.env.PRI_KEY],
+    },
+    polygontest: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.POLYGON_TEST_API_KEY}`,
+      accounts: [process.env.POLYGON_TEST_PRI_KEY],
+    },
+    polygonmain: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.POLYGON_MAIN_API_KEY}`,
+      accounts: [process.env.POLYGON_MAIN_PRI_KEY],
     },
     test: {
       url: `http://127.0.0.1:8545`,
